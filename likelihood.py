Fixed problem with frequency normalization and uneven household sizes.

likelihood.py
@@ -6,6 +6,7 @@ def counts_from_empirical(empirical, parameter_keys, baseline_only_keys=["trialn
     counts = empirical.groupby(parameter_keys + baseline_only_keys + household_keys).size()
     #import pdb; pdb.set_trace()
     counts = counts.reindex(counts.index.rename(["baseline " + key for key in parameter_keys] + baseline_only_keys + household_keys))
+    counts.name = "count"
     return counts
 
 def frequencies_from_synthetic(synthetic, parameter_keys, household_keys=["size", "infections"]):
@@ -21,9 +22,11 @@ def frequencies_from_synthetic(synthetic, parameter_keys, household_keys=["size"
     return frequencies
 
 def compute_frequencies(comparison, grouping):
-    frequency_total = len(comparison)
+    #import pdb; pdb.set_trace()
+    #frequency_total = len(comparison)
     frequencies_grouped=comparison.groupby(grouping)
-    frequencies = frequencies_grouped.size() / frequency_total
+    frequency_totals = frequencies_grouped.size().sum(level='size')
+    frequencies = frequencies_grouped.size() / frequency_totals
     frequencies.name = 'freqs'
     hh_size = frequencies.reset_index()['size'].iloc[0]
     repaired_parts = []
@@ -45,9 +48,13 @@ def compute_frequencies(comparison, grouping):
 def logl_from_data(synthetic, empirical, parameter_keys, baseline_only_keys=["trialnum"], household_keys=["size", "infections"]):
     frequencies = frequencies_from_synthetic(synthetic, parameter_keys, household_keys=household_keys)
     counts = counts_from_empirical(empirical, parameter_keys, baseline_only_keys=baseline_only_keys, household_keys=household_keys)
+    logl = logl_from_frequencies_and_counts(frequencies, counts, parameter_keys, baseline_only_keys=baseline_only_keys, household_keys=household_keys)
+    return logl
+
+def logl_from_frequencies_and_counts(frequencies, counts, parameter_keys, baseline_only_keys=["trialnum"], household_keys=["size", "infections"]):
     log_freqs = np.log(frequencies)
-    counts.name = "count"
     log_freqs.name = "log freq"
+
     counts = counts.reset_index()
     log_freqs = log_freqs.reset_index()
     log_freqs["dummy"] = 0.0
@@ -66,61 +73,3 @@ def logl_from_data(synthetic, empirical, parameter_keys, baseline_only_keys=["tr
         logls = indexed_merge['logl']
 
     return logls.sum() # summing over household keys (because they are excluded)
-
-
-
-
-
-
-
-def logl_from_frequences_and_counts(frequencies, counts):
-    if isinstance(frequencies, pd.core.series.Series): # if there are multiple sizes, the result comes out in a series, if not it comes out in a dataframe that needs the size to be stacked back in as a column
-        new_freqs = np.log(frequencies)
-    else:
-        new_freqs = np.log(frequencies).stack(level=[0,1])
-
-    counts.name = "count"
-    new_freqs.name = "freq"
-
-    # we make a table of the log frequency values, and then we just have to 'line it up' with the counts and do arithmetic, most of the work is in lining it up
-    merged = pd.merge(new_freqs.reset_index(), counts.reset_index(), on=household_keys)
-    indexed_merge = merged.set_index(["baseline " + key for key in keys] + baseline_only_keys + keys + household_keys)
-    indexed_merge["logl"] = indexed_merge["freq"] * indexed_merge["count"]
-
-    logl_df = indexed_merge.groupby(["baseline " + key for key in keys] + baseline_only_keys + keys)["logl"].sum() # summing over household keys (because they are excluded)
-    return logl_df
-
-
-
-
-
-
-
-
-
-
-
-def logl_new(baseline_df, comparison_df, keys, baseline_only_keys=["trialnum"], household_keys=["size", "infections"]):
-    comparison_grouped = comparison_df.groupby(keys) # groups by the two axes of the plots
-    frequencies = comparison_grouped.apply(lambda g: compute_frequencies(g, household_keys)) # precompute the frequencies at each point because this is the expensive step
-    print("FREQUENCIES\n", frequencies)
-    #  --- NEW METHOD ---
-    counts = baseline_df.groupby(keys + baseline_only_keys + household_keys)["model"].count()
-    counts = counts.reindex(counts.index.rename(["baseline " + key for key in keys] + baseline_only_keys + household_keys))
-
-    if isinstance(frequencies, pd.core.series.Series): # if there are multiple sizes, the result comes out in a series, if not it comes out in a dataframe that needs the size to be stacked back in as a column
-        new_freqs = np.log(frequencies)
-    else:
-        new_freqs = np.log(frequencies).stack(level=[0,1])
-
-    counts.name = "count"
-    new_freqs.name = "freq"
-
-    # we make a table of the log frequency values, and then we just have to 'line it up' with the counts and do arithmetic, most of the work is in lining it up
-    merged = pd.merge(new_freqs.reset_index(), counts.reset_index(), on=household_keys)
-    indexed_merge = merged.set_index(["baseline " + key for key in keys] + baseline_only_keys + keys + household_keys)
-    indexed_merge["logl"] = indexed_merge["freq"] * indexed_merge["count"]
-
-    logl_df = indexed_merge.groupby(["baseline " + key for key in keys] + baseline_only_keys + keys)["logl"].sum() # summing over household keys (because they are excluded)
-    return logl_df
-