011157ab2390e7ef45db910714a8a4895b39e400
Thayer <tanderson11@gmail.com>
2022-04-01 10:44:32 -0400

    Revert "Recipes tweak."
    This reverts commit 0c6511dadcbe5fe3fb5f48304db766271060f808.

--- covid_households/plot_testing.py
+++ covid_households/plot_testing.py
@@ -4,7 +4,7 @@ import numpy as np
 import recipes
 
 
-test = 'test3'
+test = 'test2'
 
 if test == 'test1':
     key_ranges = {'hsar':(0.12, .41), 'sus_mass':(0.2,0.2), 'inf_mass':None, 'size':(5,5)}
@@ -30,23 +30,6 @@ if test == 'test2':
     results = recipes.Results.load("/Users/thayer/covid_households/new_parameters/s80-p80-SAR-sizes-2-8/full_results/")
     frequency_df = results.df
     #import pdb; pdb.set_trace()
-    frequency_df = frequency_df[frequency_df.index.get_level_values('size') == 5]['frequency']
-    frequency_df = frequency_df[frequency_df.index.get_level_values('SAR') <= 0.39]
-    plotting_keys = ["p80", "SAR"]
-
-    fancy_plotting.InteractiveFigure(
-        None,
-        plotting_keys,
-        figures,
-        frequency_df=frequency_df,
-        unspoken_parameters={'s80':0.80},
-        simulation_sample_size=150,
-        figsize=(10, 4.5))
-
-
-if test == 'test3':
-    results = recipes.Results.load("/Users/thayer/covid_households/new_parameters/s80-p80-SAR-sizes-2-8/full_results/")
-    frequency_df = results.df
     frequency_df = frequency_df[frequency_df.index.get_level_values('size') == 5]['frequency']
     frequency_df = frequency_df[frequency_df.index.get_level_values('SAR') <= 0.39]
     plotting_keys = ["p80", "SAR"]
