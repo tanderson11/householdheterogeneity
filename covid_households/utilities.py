0f1bd44b63c9393abd228eca2950a5e67d7ca2b4
Thayer Anderson <tanderson11@gmail.com>
2022-07-13 11:36:39 -0400

    Broke out function definitions to top scope so small helper functions can be accessed.

--- covid_households/utilities.py
+++ covid_households/utilities.py
@@ -90,8 +90,7 @@ def lognormal_p80_solve(p80):
 def lognormal_s80_solve(s80):
     return lognormal_p80_solve(s80)
 
-from state_lengths import lognormal_DISTS
-def beta_from_sar_and_lognormal_traits(SAR, sus, inf):
+def lognormal_calculate_generalized_period(sus, inf):
     infectious_period_distribution = lognormal_DISTS[STATE.infectious]
     mu_t, sigma_t = infectious_period_distribution.mu, infectious_period_distribution.sigma
 
@@ -99,28 +98,78 @@ def beta_from_sar_and_lognormal_traits(SAR, sus, inf):
         assert sus.trait_value == 1., "Unimplemented calculation of beta with constant trait where mean != 1"
         mu_s, sigma_s = 0.,0.
     else:
+        assert isinstance(sus, traits.LognormalTrait)
         mu_s, sigma_s = sus.mu, sus.sigma
     if isinstance(inf, traits.ConstantTrait):
         assert inf.trait_value == 1., "Unimplemented calculation of beta with constant trait where mean != 1"
         mu_f, sigma_f = 0., 0.
     else:
+        assert isinstance(inf, traits.LognormalTrait)
         mu_f, sigma_f = inf.mu, inf.sigma
 
+    # because the product of lognormal random variables is itself lognormal, we introduce a 'generalized period'
+    # a random variable that behaves like a duration of the infectious state but takes into account variable susceptibility and infectivity
     mu = mu_t + mu_s + mu_f
     sigma = np.sqrt(sigma_t**2 + sigma_s**2 + sigma_f**2)
     generalized_period_rv = stats.lognorm(s=sigma, scale=np.exp(mu)) # as specified in the `scipy` documentation
+    return generalized_period_rv
 
-    def SAR_objective_function_crafter(SAR_target):
-        def SAR_objective_function(beta):
-            integral, abserror = scipy.integrate.quad(lambda x: generalized_period_rv.pdf(x) * np.exp(-1 * beta * x), 0, np.inf)
-            sar = 1 - integral
-            return np.abs(SAR_target - sar)
-        return SAR_objective_function
+def lognormal_SAR_objective_function_crafter(SAR_target, generalized_period_rv):
+    """A function that builds and returns an objective function with the signature objective_function(beta) --> residual of SAR and SAR_target.
 
-    SAR_function = SAR_objective_function_crafter(SAR)
+    Args:
+        SAR_target (float): the average SAR in the population that is desired. In other words, the value such that objective_function(SAR_target) = 0.
+        generalized_period_rv (traits.LognormalTrait): The generalization of duration of infection that encompasses the variable susceptibility and infectivity of individuals.
+
+    Returns:
+        SAR_objective_function (function): a function that returns the difference between SAR_target and measured SAR. SAR_objective_function(beta) = 0 if and only if beta (probability/time) produces a secondary attack rate = SAR_target on average.
+    """
+    def SAR_objective_function(beta):
+        integral, abserror = scipy.integrate.quad(lambda x: generalized_period_rv.pdf(x) * np.exp(-1 * beta * x), 0, np.inf)
+        sar = 1 - integral
+        return np.abs(SAR_target - sar)
+    return SAR_objective_function
+
+from state_lengths import lognormal_DISTS
+def beta_from_sar_and_lognormal_traits(SAR, sus, inf):
+    """Solves for the appropriate value of beta (probability/time of infection) given a target SAR and the distributions of susceptibility and infectivity.
+
+    Args:
+        SAR (float): the average secondary attack rate in the population (calculated in reference to households of size 2 with individuals chosen uniformly at random)
+        sus (traits.LognormalTrait): the distribution of relative susceptibility among individuals in the population (must be lognormally distributed)
+        inf (traits.LognormalTrait): the distribution of relative infectivity among individuals in the population (must be lognormally distributed)
+
+    Returns:
+       beta: the probability/time of infection between and infectious and susceptible contact that produces an average secondary attack rate equal to SAR
+    """
+    generalized_period_rv = lognormal_calculate_generalized_period(sus, inf)
+
+    SAR_function = lognormal_SAR_objective_function_crafter(SAR, generalized_period_rv)
     beta = scipy.optimize.least_squares(SAR_function, np.array((0.05)), bounds=((1.0e-6), (0.999999)))
     assert(beta.success is True)
     return beta.x[0]
+
+def calculate_residual(point):
+    s80, p80, sar = point
+    s80 = float(f"{s80:.3f}")
+    p80 = float(f"{p80:.3f}")
+    sar = float(f"{sar:.3f}")
+    if s80 == 0.8:
+        sus_dist = traits.ConstantTrait()
+    else:
+        mu, sigma = S80_P80_SAR_Inputs.s80_crib.loc[s80]
+        sus_dist = traits.LognormalTrait(mu, sigma)
+    if p80 == 0.8:
+        inf_dist = traits.ConstantTrait()
+    else:
+        mu, sigma = S80_P80_SAR_Inputs.p80_crib.loc[p80]
+        inf_dist = traits.LognormalTrait(mu, sigma)
+
+    beta = S80_P80_SAR_Inputs.beta_crib.loc[s80, p80, sar]
+    generalized_rv = lognormal_calculate_generalized_period(sus_dist, inf_dist)
+
+    objective_function = lognormal_SAR_objective_function_crafter(sar, generalized_rv)
+    return objective_function(beta)
 
 class ModelInputs(abc.ABC):
     @abc.abstractmethod
@@ -151,9 +200,12 @@ class Lognormal_Variance_Variance_Beta_Inputs(ModelInputs):
 
 class S80_P80_SAR_Inputs(ModelInputs):
     key_names = ['s80', 'p80', 'SAR']
+    # cribs are files that list precalculated mappings between these parameters and the ones required for simulation
+    # they speed up simulation by avoiding the costly numerical solving step
     s80_crib  = pd.read_csv(Path(__file__).parent / "../data/s80_lookup.csv").set_index('s80')
     p80_crib  = pd.read_csv(Path(__file__).parent / "../data/p80_lookup.csv").set_index('p80')
     beta_crib = pd.read_csv(Path(__file__).parent / "../data/beta_lookup.csv").set_index(['s80', 'p80', 'SAR'])
+    bad_combinations_crib = pd.read_csv(Path(__file__).parent / "../data/problematic_parameter_combinations.csv").set_index(['s80', 'p80', 'SAR'])
 
     def __init__(self, s80, p80, SAR) -> None:
         self.s80 = s80
@@ -182,7 +234,7 @@ class S80_P80_SAR_Inputs(ModelInputs):
                 assert(inf_variance.success is True)
                 inf_variance = inf_variance.x[0]
                 inf_dist = traits.LognormalTrait.from_natural_mean_variance(1., inf_variance)
-        
+
         if use_crib:
             beta = float(self.beta_crib.loc[((np.float(f"{self.s80:.3f}")), (np.float(f"{self.p80:.3f}")), (np.float(f"{self.SAR:.3f}")))])
         else:
