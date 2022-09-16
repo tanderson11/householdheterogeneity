cac16c98d724cb56674103b6a6944be2133a9b9f
Thayer Anderson <thayer@Thayers-MacBook-Pro.local>
2022-09-16 13:56:28 -0400

    More relative paths.

--- covid_households/plot_testing.py
+++ covid_households/plot_testing.py
@@ -1,20 +1,21 @@
 import fancy_plotting
 import numpy as np
 import recipes
+import os
+
 
 #figures = np.array(["2D only probability contour plot", "infection histograms"]).reshape((1,2))
 #figures = np.array(["2D only probability contour plot", "2D slice probability contour plot"]).reshape((1,2))
 figures = np.array(["3D slice free parameter probability contour plot"]).reshape((1,1))
 #figures = np.array(["2D only probability contour plot", "3D slice free parameter probability contour plot"]).reshape((1,2))
 
-results = recipes.Results.load('/Users/thayer/covid_households/new_parameters/gillespie-s80-p80-SAR/beta_corrections')
-#results = recipes.Results.load('/Users/thayer/covid_households/new_parameters/gillespie-s80-p80-SAR/')
+stem = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+results = recipes.Results.load(os.path.join(stem, 'new_parameters/gillespie-s80-p80-SAR/beta_corrections'))
 results.find_frequencies()
 frequency_df = results.df
 
 #frequency_df = frequency_df[(frequency_df.index.get_level_values('size') == 4) | (frequency_df.index.get_level_values('size') == 8)]
 frequency_df = frequency_df['frequency']
-#import pdb; pdb.set_trace()
 #plotting_keys = ["p80", "SAR"]
 plotting_keys = ["s80", "SAR"]
 
@@ -23,9 +24,6 @@ fancy_plotting.InteractiveFigure(
     figures,
     frequency_df,
     unspoken_parameters={'p80':0.8},
-    #simulation_population={3: 174, 4: 50, 5: 40, 6: 4, 7: 4, 8: 3}, #POP=1000
     simulation_population={3: 871, 4: 248, 5: 198, 6: 22, 7: 19, 8: 17}, #POP=5000
-    #simulation_population={3: 2612, 4: 744, 5: 595, 6: 67, 7: 57, 8: 50}, #POP=15000
-    #simulation_population={4:2000, 8:1000},
     simulation_trials=1,
     figsize=(6, 4.5))
